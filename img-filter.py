0347d95a83b23cda94d8fe05fb4f444904058c11
jin_jiankun <jiankun.jin@gmail.com>
2018-01-05 16:39:43 +0800

    update rect reader

--- img-filter.py
+++ img-filter.py
@@ -3,48 +3,44 @@ from imutils import contours
 import imutils
 import sys
 import numpy as np
+import cv2.text as text
 from matplotlib import pyplot as plt
+
 
 def main(argv):
     image_path = argv[1]
 
     image = cv2.imread(image_path, -1)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    (width, height) = image.shape[:2]
+
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     # initialize a rectangular (wider than it is tall) and square
     # structuring kernel
     rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 5))
-    sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
 
-    '''
+    gradX = gray.copy()
+
     # 1. blackhat
-    gradX = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKernel)
-    cv2.namedWindow('image',  cv2.WINDOW_NORMAL)
+    gradX = cv2.Canny(gradX, 100, 200)
     imshow(gradX)
-    '''
-    gradX = cv2.morphologyEx(gray, cv2.MORPH_OPEN, rectKernel)
-    imshow(gradX)
-
-
-
-    # 3. morph_close
-    gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
-    imshow(gradX)
-    
-
-    # 4. threshold
-    gradX = cv2.threshold(gradX, 0, 255,
-        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-    imshow(gradX)
-
-    '''
-    # 5. morph_close
     gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, sqKernel)
     imshow(gradX)
-    '''
-    '''
-    chars_image = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                    cv2.THRESH_BINARY, 11, 20)
-    '''
+
+    lines = cv2.HoughLinesP(gradX, 1, np.pi / 2, 300, None,
+                            100, 10)
+
+    lines_image = gradX.copy()
+    lines_image[:] = (255)
+
+    for i in range(0, len(lines)):
+        l = lines[i][0]
+        cv2.line(lines_image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 2, cv2.LINE_AA)
+
+    imshow(lines_image)
+
+    print("----------------------------------")
 
     '''
     RETR_EXTERNAL 	
@@ -61,18 +57,36 @@ def main(argv):
 
     RETR_FLOODFILL 	
     '''
-    im2, cnts, hierarchy = cv2.findContours(gradX, cv2.RETR_LIST,
-	                            cv2.CHAIN_APPROX_SIMPLE)
-    
-    for cnt in cnts:
+
+
+
+    im2, cnts, hierarchy = cv2.findContours(lines_image, cv2.RETR_LIST,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+    def is_acceptable_rect(cnt):
         (x, y, w, h) = cv2.boundingRect(cnt)
-        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+        return w > 20 and h > 20 and (w * h < width * height / 2)
+
+    cnts = [cnt for cnt in cnts if is_acceptable_rect(cnt)]
+
+    tess = text.OCRTesseract_create(oem=1)
+
+    for idx, cnt in enumerate(cnts):
+        (x, y, w, h) = cv2.boundingRect(cnt)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(image,'%s' % idx,(x,y), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2,cv2.LINE_AA)
         print("%s, %s, %s, %s" % (y, h, x, w))
+
+    chars_image = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                        cv2.THRESH_BINARY, 11, 20)
+    imshow(chars_image)
+
     imshow(image)
+
 
 def imshow(img):
     cv2.imshow('image', img)
     cv2.waitKey(0)
+
 
 if __name__ == "__main__":
     main(sys.argv)
